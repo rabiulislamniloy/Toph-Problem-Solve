043ce07303308fcbf7fd89de8f0f7d255f5e3fec
Rabiul Islam Niloy <154040740+rabiulislamniloy@users.noreply.github.com>
2026-01-06 15:04:30 +0600

    Implement Missing Number calculation

---
+++ Missing Number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int s;
+    int a,b,c,d;
+    cin >>s;
+    cin >>a >>b >>c;
+    d=s-(a+b+c);
+    cout <<d<< endl;
+    return 0;
+}

2b3ca9307dfa7982a168407e903aae96109ddf1d
Rabiul Islam Niloy <154040740+rabiulislamniloy@users.noreply.github.com>
2026-01-14 23:28:39 +0600

    Refactor vowel counting logic in Alayna and Vowels.cpp

---
+++ Alayna and Vowels.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;   
+    int count=0;
+    for (char c:s) 
+    {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+           count++;
+    }
+    }
+
+    cout <<count<< endl;
+    return 0;
+}
